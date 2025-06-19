972f6359378a5c6316051fceebc306bc4357ba24
Seilani Fontes <seilani.sousa2@gmail.com>
2025-06-19 19:41:35 -0300

    enviando arquivos para repositorio

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import{useTheme} from '../components/ThemeContext'
+
+
+const Header = () => {
+
+    const {theme, toggleTheme} =useTheme()
+  return (
+    <header>
+        <h1>App Theme: {theme}</h1>
+        <button onClick={toggleTheme}>  Toggle Theme:  </button>
+    </header>
+   
+  )
+}
+
+export default Header
